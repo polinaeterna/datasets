773e109e9a9dbc875b3eb01c3bd54b28a32cb292
pkazakova <pkazakova@embedika.ru>
2021-07-14 14:25:14 +0300

    add split generators and generate examples

--- datasets/russian_super_glue/russian_super_glue.py
+++ datasets/russian_super_glue/russian_super_glue.py
@@ -8,6 +8,17 @@ import os
 import datasets
 
 
+TASKS_LOWERCASED_TO_ORIGINAL = {
+    "muserc": "MuSeRC",
+    "danetqa": "DaNetQA",
+    "parus": "PARus",
+    "rcb": "RCB",
+    "russe": "RUSSE",
+    "terra": "TERRa",
+    "lidirus": "LiDiRus"
+}
+
+
 class RussianSuperGlueConfig(datasets.BuilderConfig):
     """BuilderConfig for RussianSuperGLUE."""
 
@@ -143,7 +154,136 @@ class RussianSuperGlue(datasets.GeneratorBasedBuilder):
         pass
 
     def _split_generators(self, dl_manager):
-        pass
+        dl_dir = dl_manager.download_and_extract(self.config.data_url) or ""
+        task_name = _get_task_name_from_data_url(self.config.data_url)
+        task_name = _convert_task_name(task_name)
+        dl_dir = os.path.join(dl_dir, task_name)
+        if self.config.name == "lidirus":
+            # this is a diagnostic dataset
+            return [
+                datasets.SplitGenerator(
+                    name=datasets.Split.TEST,
+                    gen_kwargs={
+                        "data_file": os.path.join(dl_dir, "{}.jsonl".format(task_name)),
+                        "split": datasets.Split.TEST,
+                    },
+                ),
+            ]
+
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={
+                    "data_file": os.path.join(dl_dir, "train.jsonl"),
+                    "split": datasets.Split.TRAIN,
+                },
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.VALIDATION,
+                gen_kwargs={
+                    "data_file": os.path.join(dl_dir, "val.jsonl"),
+                    "split": datasets.Split.VALIDATION,
+                },
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST,
+                gen_kwargs={
+                    "data_file": os.path.join(dl_dir, "test.jsonl"),
+                    "split": datasets.Split.TEST,
+                },
+            ),
+        ]
 
     def _generate_examples(self, data_file, split):
-        pass
+        with open(data_file, encoding="utf-8") as f:
+            for line in f:
+                row = json.loads(line)
+
+            if self.config.name == "muserc":
+                paragraph = row["passage"]
+                for question in paragraph["questions"]:
+                    for answer in question["answers"]:
+                        label = answer.get("label")
+                        key = "%s_%s_%s" % (row["idx"], question["idx"], answer["idx"])
+                        yield key, {
+                            "paragraph": paragraph["text"],
+                            "question": question["question"],
+                            "answer": answer["text"],
+                            "label": -1 if label is None else _cast_label(bool(label)),
+                            "idx": {
+                                "paragraph": row["idx"],
+                                "question": question["idx"],
+                                "answer": answer["idx"],
+                            },
+                        }
+
+            elif self.config.name == "rucos":
+                passage = row["passage"]
+                for qa in row["qas"]:
+                    yield qa["idx"], {
+                        "passage": passage["text"],
+                        "query": qa["query"],
+                        "entities": _get_record_entities(passage),
+                        "answers": _get_record_answers(qa),
+                        "idx": {"passage": row["idx"], "query": qa["idx"]},
+                    }
+
+            else:
+                if self.config.name == "rwsd":
+                    row.update(row["target"])
+                example = {feature: row[feature] for feature in self.config.features}
+                # TODO: check this
+                # if self.config.name == "wsc.fixed":
+                #     example = _fix_wst(example)
+
+                if "label" in row:
+                    if dataset_name == "parus":
+                        example["label"] = "choice1" if row["label"] == 0 else "choice2"
+                    else:
+                        example["label"] = _cast_label(row["label"])
+                else:
+                    assert split == datasets.Split.TEST, row
+                    example["label"] = -1
+
+                yield example["idx"], example
+
+
+def _cast_label(label):
+    """Converts the label into the appropriate string version."""
+    if isinstance(label, str):
+        return label
+    elif isinstance(label, bool):
+        return "True" if label else "False"
+    elif isinstance(label, int):
+        assert label in (0, 1)
+        return str(label)
+    else:
+        raise ValueError("Invalid label format.")
+
+
+def _get_record_entities(passage):
+    """Returns the unique set of entities."""
+    text = passage["text"]
+    entities = set()
+    for entity in passage["entities"]:
+        entities.add(text[entity["start"] : entity["end"]])
+    return sorted(entities)
+
+
+def _get_record_answers(qa):
+    """Returns the unique set of answers."""
+    if "answers" not in qa:
+        return []
+    answers = set()
+    for answer in qa["answers"]:
+        answers.add(answer["text"])
+    return sorted(answers)
+
+
+def _get_task_name_from_data_url(data_url):
+    return data_url.split("/")[-1].split(".")[0]
+
+
+# TODO: not really sure that this one is needed
+def _convert_task_name(task_name):
+    return TASKS_LOWERCASED_TO_ORIGINAL[task_name]
