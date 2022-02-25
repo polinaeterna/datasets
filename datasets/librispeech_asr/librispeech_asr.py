28c7f1620fdecfa042275b3568756506fbe899f7
polinaeterna <polina@huggingface.co>
2022-02-25 20:53:32 +0300

    fix librispeech

--- datasets/librispeech_asr/librispeech_asr.py
+++ datasets/librispeech_asr/librispeech_asr.py
@@ -15,7 +15,8 @@
 
 # Lint as: python3
 """Librispeech automatic speech recognition dataset."""
-
+import glob
+import os
 
 import datasets
 from datasets.tasks import AutomaticSpeechRecognition
@@ -101,7 +102,7 @@ class LibrispeechASR(datasets.GeneratorBasedBuilder):
             description=_DESCRIPTION,
             features=datasets.Features(
                 {
-                    "file": datasets.Value("string"),
+                    "path": datasets.Value("string"),
                     "audio": datasets.Audio(sampling_rate=16_000),
                     "text": datasets.Value("string"),
                     "speaker_id": datasets.Value("int64"),
@@ -116,39 +117,100 @@ class LibrispeechASR(datasets.GeneratorBasedBuilder):
         )
 
     def _split_generators(self, dl_manager):
+        streaming = dl_manager.is_streaming
         archive_path = dl_manager.download(_DL_URLS[self.config.name])
+        if streaming:
+            # Here we use iter_archive in streaming mode because dl_manager.download_and_extract
+            # doesn't work to stream TAR archives (we have to stream the files in the archive one by one).
+            #
+            # The iter_archive method returns an iterable of (path_within_archive, file_obj) for every
+            # file in the TAR archive.
+            #
+            archive_iterator = dl_manager.iter_archive(archive_path)
+            extracted_dir = None
+        else:
+            extracted_dir = dl_manager.extract(archive_path)
+            archive_iterator = None
 
         if self.config.name == "clean":
             train_splits = [
                 datasets.SplitGenerator(
-                    name="train.100", gen_kwargs={"files": dl_manager.iter_archive(archive_path["train.100"])}
+                    name="train.100", gen_kwargs={
+                        "archive_iterator": dl_manager.iter_archive(archive_path["train.100"]),
+                        "extracted_path": extracted_dir["train.100"] if extracted_dir else None,
+                        "streaming": streaming,
+                    }
                 ),
                 datasets.SplitGenerator(
-                    name="train.360", gen_kwargs={"files": dl_manager.iter_archive(archive_path["train.360"])}
+                    name="train.360", gen_kwargs={
+                        "archive_iterator": dl_manager.iter_archive(archive_path["train.360"]),
+                        "extracted_path": extracted_dir["train.360"] if extracted_dir else None,
+                        "streaming": streaming,
+                    }
                 ),
             ]
+
         elif self.config.name == "other":
             train_splits = [
                 datasets.SplitGenerator(
-                    name="train.500", gen_kwargs={"files": dl_manager.iter_archive(archive_path["train.500"])}
+                    name="train.500", gen_kwargs={
+                        "archive_interator": dl_manager.iter_archive(archive_path["train.500"]),
+                        "extracted_path": extracted_dir["train.500"] if extracted_dir else None,
+                        "streaming": streaming,
+                    }
                 ),
             ]
 
         return train_splits + [
             datasets.SplitGenerator(
-                name=datasets.Split.VALIDATION, gen_kwargs={"files": dl_manager.iter_archive(archive_path["dev"])}
+                name=datasets.Split.VALIDATION, gen_kwargs={
+                    "archive_iterator": dl_manager.iter_archive(archive_path["dev"]),
+                    "extracted_path": extracted_dir["dev"] if extracted_dir else None,
+                    "streaming": streaming,
+                }
             ),
             datasets.SplitGenerator(
-                name=datasets.Split.TEST, gen_kwargs={"files": dl_manager.iter_archive(archive_path["test"])}
+                name=datasets.Split.TEST, gen_kwargs={
+                    "archive_iterator": dl_manager.iter_archive(archive_path["test"]),
+                    "extracted_path": extracted_dir["test"] if extracted_dir else None,
+                    "streaming": streaming,
+                }
             ),
         ]
 
-    def _generate_examples(self, files):
+    def _generate_examples(self, archive_iterator, archive_path, streaming):
+        if streaming:
+            yield from self._generate_examples_streaming(archive_iterator)
+        else:
+            yield from self._generate_examples_non_streaming(archive_path)
+
+    def _generate_examples_non_streaming(self, extracted_path):
+        """Generate examples from a LibriSpeech archive_path."""
+        transcripts_glob = os.path.join(extracted_path, "LibriSpeech", "*/*/*/*.txt")
+        key = 0
+        for transcript_path in sorted(glob.glob(transcripts_glob)):
+            transcript_dir_path = os.path.dirname(transcript_path)
+            with open(transcript_path, "r", encoding="utf-8") as f:
+                for line in f:
+                    line = line.strip()
+                    id_, transcript = line.split(" ", 1)
+                    audio_file = f"{id_}.flac"
+                    speaker_id, chapter_id = [int(el) for el in id_.split("-")[:2]]
+                    yield key, {
+                        "id": id_,
+                        "speaker_id": speaker_id,
+                        "chapter_id": chapter_id,
+                        "path": os.path.join(transcript_dir_path, audio_file),
+                        "audio": os.path.join(transcript_dir_path, audio_file),
+                        "text": transcript,
+                    }
+
+    def _generate_examples_streaming(self, archive_iterator):
         """Generate examples from a LibriSpeech archive_path."""
         key = 0
         audio_data = {}
         transcripts = []
-        for path, f in files:
+        for path, f in archive_iterator:
             if path.endswith(".flac"):
                 id_ = path.split("/")[-1][: -len(".flac")]
                 audio_data[id_] = f.read()
@@ -164,14 +226,14 @@ class LibrispeechASR(datasets.GeneratorBasedBuilder):
                                 "id": id_,
                                 "speaker_id": speaker_id,
                                 "chapter_id": chapter_id,
-                                "file": audio_file,
+                                "path": audio_file,
                                 "text": transcript,
                             }
                         )
             if audio_data and len(audio_data) == len(transcripts):
                 for transcript in transcripts:
-                    audio = {"path": transcript["file"], "bytes": audio_data[transcript["id"]]}
-                    yield key, {"audio": audio, **transcript}
+                    audio = {"path": transcript["path"], "bytes": audio_data[transcript["id"]]}
+                    yield key, {"audio": audio, "path": None, **transcript}
                     key += 1
                 audio_data = {}
                 transcripts = []
